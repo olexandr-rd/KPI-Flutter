6b1f8daa8a8ce8c3294007d2b24f521f6401132d
sasha <sasha2002rudenko@gmail.com>
2025-02-15 19:21:42 +0200

    practice 2

---
+++ lib/practice2/solid_particles.dart
@@ -0,0 +1,17 @@
+class SolidParticles {
+  final double lhv;
+  final double vol;
+  final double ash;
+  final double mass;
+  final double ashCapture;
+  final double emissions;
+
+  SolidParticles({
+    required this.lhv,
+    required this.vol,
+    required this.ash,
+    required this.mass,
+    required this.ashCapture,
+    required this.emissions,
+  });
+}
